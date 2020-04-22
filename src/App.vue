99c6caf67bdb33b11be303045eee51b87512604a
Matthew Poruben <poruben.matus@gmail.com>
2020-04-22 13:08:22 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <template>
   <ion-app>
-    <Header/>
-    <Messages/>
-    <Bottom :emojis="['🔥', '🙂', '💸']" />
   </ion-app>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Messages from './components/Messages.vue'
-import Bottom from './components/Bottom.vue'
-
 import MessagesPlugin from '@/plugins/messages.js'
 
 export default {
@@ -19,12 +12,5 @@ export default {
   provide: {
     messagesService: new MessagesPlugin()
   },
-
-  components: {
-    Header,
-    Messages,
-    Bottom
-  },
-  
 }
 </script>
